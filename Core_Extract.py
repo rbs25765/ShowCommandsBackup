b2cc4579a528e7fec5f4f2a011373631fdfefca5
rbs25765 <47831086+rbs25765@users.noreply.github.com>
2019-05-12 01:50:08 +0530

    config_extract with netconnec

--- Core_Extract.py
+++ Core_Extract.py
@@ -1,5 +1,6 @@
 from netmiko import ConnectHandler
 from IP_CMD import IPCmd
+import os
 
 # from easygui import passwordbox
 
@@ -8,6 +9,10 @@ class DeviceConnect:
 
     def __init__(self):
         self.device_data = {}
+        self.ic = IPCmd()
+        self.commands = self.ic.cmd_extract()
+        self.device_type = self.ic.device_type_extract()
+        self.device_config = []
 
     def dev_data(self, ip, dev_type):
 
@@ -21,24 +26,37 @@ class DeviceConnect:
         # self.device_data['password'] = passwordbox("Enter Password: ")
         # self.device_data['secret'] = passwordbox("Enter Secret: ")
         # self.device_data['secret'] = input("Enter secret: ")
+        # return self.device_data
 
     def dev_connect(self):
-        net_connect = ConnectHandler(**self.device_data)
-        return net_connect
+        print("Connecting to device...")
+        net_con = ConnectHandler(**self.device_data)
+        return net_con
 
-    def print_data(self):
-        # print(self.core_ip, self.show_commands)
-        ipc = IPCmd()
-        device_type = ipc.device_type_extract()
-        self.dev_data('192.168.2.131', device_type)
-        dev_status = self.dev_connect()
-        print(dev_status.find_prompt())
+    def config_extract(self, ip='192.168.2.131'):
+        self.dev_data(ip, self.device_type)
+        net_connect = self.dev_connect()
+        net_connect.enable()
+        if net_connect.enable():
+            print("Successfully connected, working on capturing configurations")
+        for cmd in self.commands:
+            config_output = net_connect.send_command_timing(cmd, delay_factor=2)
+            self.device_config.append(config_output)
+        print("Successfully Completed")
+        return self.device_config
+
+    def file_saving(self):
+        for data in self.device_config:
+            with open('./Output/'+self.device_data['ip']+'.txt','a+') as f:
+                f.write(data)
 
 
 if __name__ == '__main__':
 
     dc = DeviceConnect()
-    dc.print_data()
+    dc.config_extract()
+    dc.file_saving()
+    # print(dc.device_config)
 
 
 
