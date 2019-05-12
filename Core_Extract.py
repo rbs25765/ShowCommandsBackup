f86292b92bc4459356a097c1c5a59eb83aef4e43
rbs25765 <47831086+rbs25765@users.noreply.github.com>
2019-05-12 11:42:06 +0530

    modified dictionary input and coreExtract

--- Core_Extract.py
+++ Core_Extract.py
@@ -1,7 +1,6 @@
-from netmiko import ConnectHandler
+from netmiko import ConnectHandler, NetMikoTimeoutException, NetMikoAuthenticationException
 from IP_CMD import IPCmd
 import os
-
 # from easygui import passwordbox
 
 
@@ -14,40 +13,56 @@ class DeviceConnect:
         self.device_type = self.ic.device_type_extract()
         self.device_config = []
 
-    def dev_data(self, ip, dev_type):
+    def dev_data(self, ip):
 
         self.device_data['ip'] = ip
-        self.device_data['username'] = 'cisco'
-        self.device_data['password'] = 'cisco'
-        self.device_data['secret'] = 'cisco'
-        self.device_data['device_type'] = dev_type
-        # self.device_data['username'] = input("Enter Username: ")
-        # self.device_data['username'] = input("Enter Password: ")
+        # self.device_data['username'] = 'cisco'
+        # self.device_data['password'] = 'cisco'
+        # self.device_data['secret'] = 'cisco'
+        self.device_data['device_type'] = self.device_type
+        self.device_data['username'] = input("Enter Username: ")
+        self.device_data['password'] = input("Enter Password: ")
         # self.device_data['password'] = passwordbox("Enter Password: ")
         # self.device_data['secret'] = passwordbox("Enter Secret: ")
-        # self.device_data['secret'] = input("Enter secret: ")
-        # return self.device_data
+        self.device_data['secret'] = input("Enter secret: ")
+        return self.device_data
 
-    def dev_connect(self):
-        print("Connecting to device...")
-        net_con = ConnectHandler(**self.device_data)
-        return net_con
+    def dev_connect(self, ip):
+        self.dev_data(ip)
+        i = 0
+        while i <3:
+            try:
+                print("Connecting to device...")
+                net_con = ConnectHandler(**self.device_data)
+                print(self.device_data)
+                print("Device Connected, executing show commands")
+                return net_con
+            except NetMikoTimeoutException:
+                print(ip+" Device not accessible")
+                return None
+            except NetMikoAuthenticationException:
+                print("Invalid credentials, please try again")
+                self.dev_data(ip)
+                i += 1
+                if i == 3:
+                    print("Maximum entries tried")
 
-    def config_extract(self, ip='192.168.2.131'):
-        self.dev_data(ip, self.device_type)
-        net_connect = self.dev_connect()
-        net_connect.enable()
-        if net_connect.enable():
-            print("Successfully connected, working on capturing configurations")
-        for cmd in self.commands:
-            config_output = net_connect.send_command_timing(cmd, delay_factor=2)
-            self.device_config.append(config_output)
-        print("Successfully Completed")
-        return self.device_config
+    def config_extract(self, ip='192.168.2.142'):
+        net_connect = self.dev_connect(ip)
+        if net_connect is not None:
+            net_connect.enable()
+            for cmd in self.commands:
+                config_output = net_connect.send_command_timing(cmd, delay_factor=2)
+                self.device_config.append((cmd, config_output))
+            print("Command Execution Successfully Completed")
+            return self.device_config
 
     def file_saving(self):
-        for data in self.device_config:
-            with open('./Output/'+self.device_data['ip']+'.txt','a+') as f:
+        for command, data in self.device_config:
+            with open('./Output/'+self.device_data['ip']+'.txt', 'a+') as f:
+                f.write('\n')
+                f.write(command)
+                f.write('\n')
                 f.write(data)
 
 
